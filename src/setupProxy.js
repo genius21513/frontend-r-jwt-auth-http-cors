2d5d98d93336bdbeec97c3101e78146906ffd082
Genius <genius21513@gmail.com>
2023-01-19 19:21:44 +0900

    Change code

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,8 @@ module.exports = function (app) {
     "/api",
     createProxyMiddleware({
       // target: "http://localhost:8080",
-      target: "http://10.10.13.227:8080",
+      // target: "http://10.10.13.227:8080",
+      target: "http://127.0.0.1:8080",
       changeOrigin: true,
     })
   );

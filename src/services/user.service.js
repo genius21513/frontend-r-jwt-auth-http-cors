340275d702cf26c8f1e87637b81d090830c556ef
Genius <genius21513@gmail.com>
2023-01-21 15:49:46 +0900

    Set Proxy

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -8,9 +8,9 @@ import axios from "axios";
 // const API_URL = "http://10.10.13.227:3000/api/test/";
 // const API_URL = "http://127.0.0.1:3000/api/test/";
 
-// const API_URL = "/api/test/";
+const API_URL = "/api/test/";
 
-const API_URL = "http://13.53.64.138:8080/api/test/";
+// const API_URL = "http://13.53.64.138:8080/api/test/";
 
 const getPublicContent = () => {
   return axios.get(API_URL + "all");

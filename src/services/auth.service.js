2603fbe180ca6107df1bc342bbc197a2cfa567c9
Genius <genius21513@gmail.com>
2023-01-21 15:57:26 +0900

    Update apis

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_URL = "http://13.53.64.138:8080/api/auth/";
-const API_URL = "/api/auth/";
+const API_URL = "http://13.53.64.138:8080/api/auth/";
+// const API_URL = "/api/auth/";
 
 const register = (username, email, password) => {
   return axios.post(API_URL + "signup", {
